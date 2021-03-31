3dd2e8f3ad3e92084dd82d27d7425780a87a318f
nurbs999 <nurbson@gmail.com>
2021-03-31 22:52:25 +0200

    Add error messages on failed sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
+  def create
+    @user = User.new(user_params)
+    render 'new' unless @user.save
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
