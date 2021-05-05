6d00c377521ac78a6aacdd4f0220e85940e39820
nurbs999 <nurbson@gmail.com>
2021-05-05 20:38:43 +0200

    Implement basic login/logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,6 +14,7 @@ module SessionsHelper
   end
 
   def log_out
-    session[:user_id] = nil
+    reset_session
+    @current_user = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
               uniqueness: true
   validates :password, length: {minimum: 6}, presence: true
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : Bcryp::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
